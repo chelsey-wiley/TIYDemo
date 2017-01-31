0c4e7844337483e747c57bc1f72846023e8564ad
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2017-01-31 14:26:49 -0600

    cleaned up code, re organized functions

--- liveVideo/public/all.js
+++ liveVideo/public/all.js
@@ -598,33 +598,6 @@ if (window.MR === undefined) {
     }
 
     _createClass(SignLanguageRecordingComponent, [{
-      key: "theRecording",
-      value: function theRecording(stream) {
-        //turn this into an if else statement with text content and toggle?
-        console.log('startRecording function');
-        this.getit(stream);
-        var mediaRecorder = new MediaRecorder(stream);
-        this.mediaRecorder = mediaRecorder;
-
-        if (this.mediaRecorder.state !== 'recording') {
-          console.log('Created MediaRecorder', mediaRecorder);
-          this.mediaRecorder.start();
-          console.log('MediaRecorder state:', this.mediaRecorder.state);
-          this.mediaRecorder.ondataavailable = this.handleDataAvailable;
-        }
-      }
-    }, {
-      key: "handleDataAvailable",
-      value: function handleDataAvailable(stream) {
-
-        if (this.recordedBlobs === undefined) {
-          this.recordedBlobs = [];
-        };
-        if (stream.data && stream.data.size > 0) {
-          this.recordedBlobs.push(stream.data);
-        }
-      }
-    }, {
       key: "error",
       value: function error() {
         console.log('error');
@@ -641,8 +614,8 @@ if (window.MR === undefined) {
         }
       }
     }, {
-      key: "getit",
-      value: function getit(stream) {
+      key: "getStream",
+      value: function getStream(stream) {
         var videoStream = stream;
         video.src = createSrc(stream);
         video.play();
@@ -658,21 +631,28 @@ if (window.MR === undefined) {
         console.log('clicked record');
       }
     }, {
-      key: "clickPause",
-      value: function clickPause() {
-        console.log('clicked Pause');
-        video.pause();
+      key: "theRecording",
+      value: function theRecording(stream) {
+        console.log('startRecording function');
+        this.getStream(stream);
+        var mediaRecorder = new MediaRecorder(stream);
+        this.mediaRecorder = mediaRecorder;
+        if (this.mediaRecorder.state !== 'recording') {
+          console.log('Created MediaRecorder', mediaRecorder);
+          this.mediaRecorder.start();
+          console.log('MediaRecorder state:', this.mediaRecorder.state);
+          this.mediaRecorder.ondataavailable = this.handleDataAvailable;
+        }
       }
     }, {
-      key: "clickResume",
-      value: function clickResume() {
-        console.log('clicked resume');
-      }
-    }, {
-      key: "clickPlay",
-      value: function clickPlay() {
-        console.log('clicked play');
-        video.src = window.URL.createObjectURL(this.superBlob);
+      key: "handleDataAvailable",
+      value: function handleDataAvailable(stream) {
+        if (this.recordedBlobs === undefined) {
+          this.recordedBlobs = [];
+        };
+        if (stream.data && stream.data.size > 0) {
+          this.recordedBlobs.push(stream.data);
+        }
       }
     }, {
       key: "clickStop",
@@ -685,6 +665,12 @@ if (window.MR === undefined) {
         var superBlob = new Blob(this.mediaRecorder.recordedBlobs, { type: 'video/webm' });
         this.superBlob = superBlob;
         console.log('superBlob:', this.superBlob);
+      }
+    }, {
+      key: "clickPlay",
+      value: function clickPlay() {
+        console.log('clicked play');
+        video.src = window.URL.createObjectURL(this.superBlob);
       }
     }, {
       key: "clickDownload",
@@ -744,58 +730,48 @@ if (window.MR === undefined) {
             { className: "recorder-container" },
             React.createElement(
               "h1",
-              null,
-              "Recorder"
+              { className: "page-title" },
+              "Sign Language Recorder"
             ),
             React.createElement("video", { id: "video", controls: true, autoPlay: true }),
             React.createElement(
-              "button",
-              { id: "supported", onClick: function onClick() {
-                  _this3.clickSupport();
-                } },
-              "Support"
-            ),
-            React.createElement(
-              "button",
-              { id: "record", onClick: function onClick() {
-                  _this3.clickRec();
-                } },
-              "Record"
-            ),
-            React.createElement(
-              "button",
-              { id: "pause", onClick: function onClick() {
-                  _this3.clickPause();
-                } },
-              "Pause"
-            ),
-            React.createElement(
-              "button",
-              { id: "resume", onClick: function onClick() {
-                  _this3.clickResume();
-                } },
-              "Resume"
-            ),
-            React.createElement(
-              "button",
-              { id: "stop", onClick: function onClick() {
-                  _this3.clickStop();
-                } },
-              "Stop"
-            ),
-            React.createElement(
-              "button",
-              { id: "play", onClick: function onClick() {
-                  _this3.clickPlay();
-                } },
-              "Play"
-            ),
-            React.createElement(
-              "button",
-              { id: "download", onClick: function onClick() {
-                  _this3.clickDownload();
-                } },
-              "Download"
+              "div",
+              { className: "video-buttons" },
+              React.createElement(
+                "button",
+                { id: "supported", onClick: function onClick() {
+                    _this3.clickSupport();
+                  } },
+                "Support"
+              ),
+              React.createElement(
+                "button",
+                { id: "record", onClick: function onClick() {
+                    _this3.clickRec();
+                  } },
+                "Record"
+              ),
+              React.createElement(
+                "button",
+                { id: "stop", onClick: function onClick() {
+                    _this3.clickStop();
+                  } },
+                "Stop"
+              ),
+              React.createElement(
+                "button",
+                { id: "play", onClick: function onClick() {
+                    _this3.clickPlay();
+                  } },
+                "Play"
+              ),
+              React.createElement(
+                "button",
+                { id: "download", onClick: function onClick() {
+                    _this3.clickDownload();
+                  } },
+                "Download"
+              )
             )
           ),
           React.createElement(MR.YoutubeComponent, null)
